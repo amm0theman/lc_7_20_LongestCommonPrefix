af199ae53f43143e06611ec02d28310795e42adf
amm0theman <ammonriley.97@gmail.com>
2023-07-20 21:56:31 -0600

    First leetcode challenge complete! Faster than 100% of c# users, but heavy on memory.

---
+++ lc_7_20_LongestCommonPrefix/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        string returnString = strs.Length > 0 ? strs[0] : "";
+
+        for (int i = 0; i < strs.Length - 1; i++)
+        {
+            returnString = GetCommonPrefix(returnString, strs[i + 1]);
+            if (returnString.Length == 0)
+            {
+                return "";
+            }
+        }
+
+        if (strs.Length == 1)
+        {
+            return strs[0];
+        }
+
+        return returnString;
+    }
+
+    public string GetCommonPrefix(string firstWord, string secondWord)
+    {
+        string prefix = "";
+
+        for (int i = 0; (i < firstWord.Length) && (i < secondWord.Length); i++)
+        {
+            if (firstWord[i] == secondWord[i])
+            {
+                prefix += firstWord[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return prefix;
+    }
+}
